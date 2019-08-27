c944956eb92a12b6eadc68d7dbbc187fe9fc1293
Adrain <huangmj@epb360.com>
2019-08-28 00:26:25 +0800

    springboot结合testNg

---
+++ ad-testng-spring-boot-sample/src/main/java/com/adrain/AdTestngSpringBootSampleApplication.java
@@ -0,0 +1,13 @@
+package com.adrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdTestngSpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdTestngSpringBootSampleApplication.class, args);
+	}
+
+}
